90faee57db87d67b28421beab6b1dc35a2c04951
EthanNguyen-Tu <101310286+EthanNguyen-Tu@users.noreply.github.com>
2025-02-21 09:33:42 -0500

    feat(HeaderBar): Color change on scroll

--- cookbook-website/src/components/HeaderBar/HeaderBar.tsx
+++ cookbook-website/src/components/HeaderBar/HeaderBar.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./HeaderBar.css";
 import DropMenuButton from "../DropMenuButton/DropMenuButton.tsx";
 
 function HeaderBar() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <div className="HeaderBar">
+        <div id="HeaderBar" className={scrolled ? "opaque" : ""}>
             <DropMenuButton style={{ marginLeft: "10px", marginTop: "10px" }} />
         </div>
     );
